546f0e616a4fdb9ada7e4bbcced291a293bd2262
ps <pengshuai03@meituan.com>
2017-12-27 15:00:12 +0800

    Widget 原始demo
    创建新的工程，选择墨墨的图标作为临时图标
    增加一段文字作为Widget 的测试文字

---
+++ app/src/main/java/com/mt/android/finance/miuitargetwidget/MainUIActivity.java
@@ -0,0 +1,13 @@
+package com.mt.android.finance.miuitargetwidget;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainUIActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_ui);
+    }
+}
